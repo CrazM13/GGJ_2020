1644de9977ae21d13aff5f6bf43e302b476dbd21
wteixeira <willrteixeira@gmail.com>
2020-02-01 15:07:01 -0500

    moar textures! credits scene, stat scene and panels, new repair cursor, random name generator

---
+++ GGJ_Repair_2D/Assets/_Scripts/UI/CharacterStatPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStatPanel : MonoBehaviour
+{
+    public int characterID;
+    public Text statOutput;
+    public Text nameLabel;
+    public Image portraitHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildStatOutput();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BuildStatOutput()
+    {
+        string output = "";
+
+
+        // TODO: Stats!!!!!
+
+
+        statOutput.text = output;
+    }
+}
---
+++ GGJ_Repair_2D/Assets/_Scripts/UI/RandomNameGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RandomNameGenerator", menuName = "RandomNameGenerator", order = 99)]
+public class RandomNameGenerator : ScriptableObject
+{
+    public TextAsset nameTable;
+
+    #region Singleton Setup
+    private static RandomNameGenerator activeInstance;
+    public static RandomNameGenerator Instance
+    {
+        get
+        {
+            if (activeInstance == null)
+            {
+                activeInstance = Resources.Load<RandomNameGenerator>("RandomNameGenerator");
+            }
+            return activeInstance;
+        }
+    }
+    #endregion Singleton Setup
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static string GetRandomName()
+    {
+        string[] names = Instance.nameTable.text.Split('\n');
+
+        return names[Random.Range(0, names.Length)];
+    }
+}
