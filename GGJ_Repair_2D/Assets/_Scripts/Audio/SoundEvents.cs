88d61e7905a5f8b0fc1a8302098f14ca076f57d0
wteixeira <willrteixeira@gmail.com>
2020-02-01 21:50:47 -0500

    Sounds!! All sounds can be played in 1 line of code. SoundSystem.Instance.PlaySound and just pass the event ID in.

---
+++ GGJ_Repair_2D/Assets/_Scripts/Audio/SoundEvents.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundEvents
+{
+    Invalid,
+    Select,
+    Deselect,
+    FloodCreated,
+    FloodDestroyed,
+    FireCreated,
+    FireDestroyed,
+    DiseaseCreated,
+    DiseaseRepaired,
+    Repair,
+    LevelStart,
+    LevelComplete,
+    DisasterRepairFailure,
+    Yay,
+    H1SelectGreet,
+    H1Deselect,
+    H1Confirm,
+    H1Death,
+    H1Move,
+
+    H2SelectGreet,
+    H2Deselect,
+    H2Confirm,
+    H2Death,
+    H2Move,
+
+    H3SelectGreet,
+    H3Deselect,
+    H3Confirm,
+    H3Death,
+    H3Move,
+
+    H4SelectGreet,
+    H4Deselect,
+    H4Confirm,
+    H4Death,
+    H4Move,
+
+
+}
---
+++ GGJ_Repair_2D/Assets/_Scripts/Audio/SoundSystem.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSystem : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    [System.Serializable]
+    public class SoundEventData
+    {
+        public SoundEvents ID;
+        public List<AudioClip> clips;
+    }
+
+    public List<SoundEventData> allSoundEvents = new List<SoundEventData>();
+
+    // fast access database
+    private Dictionary<SoundEvents, SoundEventData> database = new Dictionary<SoundEvents, SoundEventData>();
+
+
+    #region Singleton Setup
+    private static SoundSystem activeInstance;
+    public static SoundSystem Instance
+    {
+        get
+        {
+            if (activeInstance == null)
+            {
+                activeInstance = (GameObject.Instantiate<GameObject>( Resources.Load<GameObject>("PR_SoundSystem"))).GetComponent<SoundSystem>();
+                activeInstance.BuildDatabase();
+            }
+            return activeInstance;
+        }
+    }
+    #endregion Singleton Setup
+
+    void BuildDatabase()
+    {
+        foreach(SoundEventData eventX in allSoundEvents)
+        {
+            if(!database.ContainsKey(eventX.ID))
+            {
+                database.Add(eventX.ID, eventX);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound(SoundEvents i_ID)
+    {
+        SoundEventData dataForSound;
+        if(database.TryGetValue(i_ID, out dataForSound))
+        {
+            if (dataForSound.clips.Count > 0)
+            {
+                AudioClip clip = dataForSound.clips[Random.Range(0, dataForSound.clips.Count)];
+                if (clip)
+                {
+                    audioSource.PlayOneShot(clip);
+                }
+            }
+        }
+    }
+}
